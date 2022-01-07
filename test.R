c0dc434c13386fafbfedf0c9633e677fefcf4a13
Michael Mayer <michael.mayer@rstudio.com>
2022-01-07 16:35:55 +0000

    - cleanup and more documentation - adding iteration at the end to find valid snapshot

--- test.R
+++ test.R
@@ -1,8 +1,30 @@
+# Please change the three lines below according to your requirements
+
+# CRAN repo to use
+CRANRepo<-"https://packagemanager.rstudio.com/cran/__linux__/focal/latest/"
+
+# Base Path where additional packages for RStudio integration should be installed 
+newlibPAth<-"/tmp/additional-packages"
+
+# RStudio version
+rstudiover<-"v2021.09.0+351"
+
+########## Main code ############
+
+# set CRAN repo
+r<-options()$repos
+r["CRAN"]<-CRANRepo
+options(repos=r)
+
+
 # reset libpath to the bare minimum
 .libPaths(paste0(Sys.getenv("R_HOME"),"/library"))
 
-# temporarily install requirements for this code into ./packages/tmp
+# temporarily install requirements to run this code into ./packages/tmp
 tmppath="./packages/tmp"
+if (dir.exists("./packages/tmp")) { 
+  unlink("./packages/tmp",recursive = TRUE)
+  }
 dir.create(tmppath, recursive=TRUE)
 install.packages(c("RCurl","curl","selectr","tibble", "jsonlite","data.table","rvest","anytime","stringr"),lib = tmppath)
 
@@ -35,18 +57,9 @@ getpackagetime <- function(packagename,packageversion) {
   }
 }
 
-
-
-#https://raw.githubusercontent.com/rstudio/rstudio/main/src/cpp/session/resources/dependencies/r-packages.json
-
-URL<-"https://raw.githubusercontent.com/rstudio/rstudio/v2021.09.1%2B372/src/cpp/session/resources/dependencies/r-packages.json"
+# URL where the requirements for R packages are being kept
+URL<-paste0("https://raw.githubusercontent.com/rstudio/rstudio/",rstudiover,"/src/cpp/session/resources/dependencies/r-packages.json")
 jsondata <- jsonlite::fromJSON(URL,simplifyDataFrame = TRUE)
-
-#+0*seq(1,length(jsondata$packages))
-# extend libPath
-
-
-
 
 mylist <- c()
 namelist <- c()
@@ -59,20 +72,51 @@ for (i in seq(1,length(jsondata$packages))) {
   mydate<-format(as.Date(getpackagetime(name,version)))
   mylist <- c(mylist,mydate)
   namelist <- c(namelist, name)
-  print(mylist)
 }
 
 latestdate <- max(mylist[!is.na(mylist)])
 
-snapshotrepo<-gsub("latest",format(as.Date(latestdate), format="%Y-%m-%d"),options()$repos["CRAN"])
-
-destpath=paste0("./packages/needed/",R.version$major,".",substr(R.version$minor,0,1))
+destpath=paste0(newlibPAth,"/",R.version$major,".",substr(R.version$minor,0,1))
 
 # reset libpath to the bare minimum
 .libPaths(paste0(Sys.getenv("R_HOME"),"/library"))
 
+if (dir.exists(destpath)) {
+  unlink(destpath)
+}
 dir.create(destpath,recursive=TRUE)
 
 .libPaths(c(paste0(Sys.getenv("R_HOME"),"/library"),destpath))
-install.packages(namelist, repos=snapshotrepo,quiet=FALSE, upgrade="never",lib=destpath)
 
+# Attempt to install packages from snapshot - if snapshot does not exist, increase day by 1 and try again
+install_packages <- function(packages,latestdate,destpath){
+  
+  snapshotrepo<-gsub("latest",format(as.Date(latestdate)+1, format="%Y-%m-%d"),options()$repos["CRAN"])
+  
+  tryCatch(
+    expr = {
+      install.packages(packages, repos=snapshotrepo,quiet=FALSE, upgrade="never",lib=destpath)
+    },
+    error = function(e){
+      message('Caught an error!')
+      print(e)
+    },
+    warning = function(w){
+      message('Caught an error!')
+      print(w)
+      install_packages(packages, as.Date(latestdate)+1, destpath)
+    },
+    finally = {
+      message('All done, quitting.')
+    }
+  )    
+}
+
+install_packages(namelist, latestdate, destpath)
+
+# Delete temp path 
+unlink("./packages/tmp",recursive = TRUE)
+
+# Finally end with the instructions to add a line to Rprofile.site
+cat("\n\nInstallation successfuly ! \n")
+cat(paste0("IMPORTANT !!!\nNow please add \".libPaths(", destpath,")\" to ", Sys.getenv("R_HOME"),"/Rprofile.site \n"))
